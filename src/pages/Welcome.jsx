1971e98ebb7d85c5e8ad470e78f211e445fda378
Muhammed Sinan P <iam.muhammedsinan.p@gmail.com>
2024-09-14 14:43:00 +0530

    complete ecommerse project

---
+++ src/pages/Welcome.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Welcome = () => {
+  return (
+    <div className='min-h-screen flex flex-col'>
+        <img src='../src/assets/Greatdiscount.jpg'/>
+        <img src='../src/assets/big sale.jpg'/>
+        <img src='../src/assets/men banner.jpg'/>
+        <img src='../src/assets/Women banner.jpg'/>
+        <img src='../src/assets/jewelery.jpg'/>
+        <img src='../src/assets/electronics.jpg'/>
+
+    </div>
+  )
+}
+
+export default Welcome
---
+++ src/store/authentication.js
@@ -0,0 +1,19 @@
+
+import {createSlice} from "@reduxjs/toolkit"
+
+const INITIAL_STATUS = {
+ loginStatus : false,
+}
+
+const authSlice = createSlice({
+    name: "log",
+    initialState: INITIAL_STATUS,
+    reducers:{
+        updateLoginStatus : (state,action)=>{
+            state.loginStatus = !state.loginStatus;
+        },
+    }
+})
+
+export const {updateLoginStatus} = authSlice.actions;
+export default authSlice.reducer;
